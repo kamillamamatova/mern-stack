756c39013d62410bbc397562d615ecaf9a6ae19f
kamillamamatova <kamillamamatova.km@gmail.com>
2026-02-06 22:12:47 -0500

    Added .gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // express app
@@ -9,6 +11,8 @@ app.get('/', (req, res) => {
 })
 
 // Listen for requests
-app.listen(4000, () => {
-    console.log('Listening on port 4000')
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port')
 })
+
+process.env
