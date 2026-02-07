3f6231a921055eca272e283506720983c4bc9ccd
kamillamamatova <kamillamamatova.km@gmail.com>
2026-02-06 23:40:54 -0500

    Added middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const express = require('express')
 // express app
 const app = express()
 
+// Middleware
+// next needs to be invoked to move onto app.get
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
 // Routes
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to the app'})
