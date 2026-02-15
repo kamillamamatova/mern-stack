542be5a2caf662c71bcfaf401cd3e2a19023743c
kamillamamatova <kamillamamatova.km@gmail.com>
2026-02-15 00:00:14 -0500

    Added something that keeps local states in sync with the database

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,10 +1,30 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
 
 export const WorkoutContext = createContext()
 
+export const workoutsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WORKOUTS':
+            return {
+                workouts: action.payload
+            }
+        case 'CREATE_WORKOUT':
+            return {
+                workouts: [action.payload, ...state.workouts]
+            }
+        default:
+            return state
+    }
+}
+
 export const WorkoutContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(workoutReducer, {
+        workouts: null
+        
+    })
+
     return(
-        <WorkoutContext.Provider>
+        <WorkoutContext.Provider value={}>
             { children }
         </WorkoutContext.Provider>
     )
