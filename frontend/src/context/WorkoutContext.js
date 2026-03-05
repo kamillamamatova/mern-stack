10c7354cbfd11e6422e6a448fcc487a104d66d0e
kamillamamatova <kamillamamatova.km@gmail.com>
2026-03-04 19:59:38 -0500

    Fixed spelling error

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -18,9 +18,8 @@ export const workoutsReducer = (state, action) => {
 }
 
 export const WorkoutContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(workoutReducer, {
+    const [state, dispatch] = useReducer(workoutsReducer, {
         workouts: null
-        
     })
 
     return(
